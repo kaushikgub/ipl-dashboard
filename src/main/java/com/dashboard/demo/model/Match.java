73de6abd216945f2143c1a05fffb715495266d45
kaushikgub <kaushik.gub@gmail.com>
2021-04-23 19:19:49 +0600

    first commit

---
+++ src/main/java/com/dashboard/demo/model/Match.java
@@ -0,0 +1,28 @@
+package com.dashboard.demo.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@ToString
+public class Match {
+    private Long id;
+    private String city;
+    private LocalDate date;
+    private String playerOfMatch;
+    private String venue;
+    private String team1;
+    private String team2;
+    private String tossWinner;
+    private String tossDecision;
+    private String matchWinner;
+    private String result;
+    private String resultMargin;
+    private String method;
+    private String umpire1;
+    private String umpire2;
+}
